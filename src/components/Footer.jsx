c1109906793e36e024e98f44d11fee70f4c7c5f1
kq1l <d.mmr@inbox.ru>
2025-03-20 12:34:56 +0900

    Практика 4
    Стилизовать футер и хедер

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,14 @@
-import React from "react";
-
+import React from 'react';
 
 const footer = () =>{
     return(
         <>
-        <div className="fixed bottom-0 flex items-start w-full">
+        <div className="fixed bottom-0 flex items-start w-full bg-blue-50 p-4 mt-8">
             
             
             <div className="columns-[20vw] ml-5">
                 
-                    <a href="soglashenie">Пользовательское соглашение</a><br/>
+                    <a href="soglashenie" className='text-gray-800 hover:text-blue-500 transition duration-300'>Пользовательское соглашение</a><br/>
                     <a href="politika">Политика конфиденциальности</a>
             </div>
             <div className="columns-[30vw]">
